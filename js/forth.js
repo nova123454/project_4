098e6214da2314f9d8a79668731ccbed1eaa57bc
nova123454 <139088342+nova123454@users.noreply.github.com>
2023-09-13 17:42:02 +0900

    2023-09-13

--- js/forth.js
+++ js/forth.js
@@ -26,9 +26,15 @@ $(function () {
     let idx=$(this).index();
     console.log(idx)
 
-    $(".aladin_first_middle .sub").eq(idx).fadeIn().siblings('.sub').hide();
+    $(".aladin_first_middle .sub").eq(idx).fadeIn().css('display',"flex").siblings('.sub').hide();
   });
 
+
+
+
+  
+
+  
   // $('.aladin_hoverbox:nth-of-type(2)').click(function () {
   //   $(".aladin_first_middlesub1").fadeIn().siblings('.sub').hide();
   // });
@@ -99,7 +105,7 @@ $.ajax({
 
     $(".imgbox")
       .eq(i)
-      .append("<button>" + "click" + "</button>");
+      .append("<button>" + "장바구니 담기" + "</button>");
   }
 });
 
@@ -125,3 +131,317 @@ $.ajax({
 //     $(".imgbox").append("<button>" + "click" + "</button>");
 
 // });
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "뇌과학" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox1")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox1")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox1")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox1")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "오펜하이머" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox2")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox2")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox2")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox2")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "과학고전" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox3")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox3")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox3")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox3")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "죽음" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox4")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox4")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox4")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox4")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "세계사" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox5")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox5")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox5")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox5")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "쇼펜하우어" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox6")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox6")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox6")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox6")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "니체" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox7")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox7")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox7")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox7")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "마르크스" },
+  headers: { Authorization: "KakaoAK 45941751e295b1ee1621bc1864a3716e" },
+}).done(function (msg) {
+  console.log(msg);
+
+  let origin = msg.documents;
+
+  //썸네일이 빈 문자열인것은 제외
+  let data = origin.filter((val) => {
+    return val.thumbnail != "";
+  });
+
+  console.log(data);
+
+  for (let i = 0; i < 4; i++) {
+    $(".imgbox8")
+      .eq(i)
+      .append("<img src='" + data[i].thumbnail + "'/>");
+    $(".imgbox8")
+      .eq(i)
+      .append("<h3>" + data[i].title + "</h3>");
+
+    var str = data[i].contents;
+    var str2 = str.substring(0, 0);
+
+    $(".imgbox8")
+      .eq(i)
+      .append("<p>" + str2 + "</p>");
+
+    $(".imgbox8")
+      .eq(i)
+      .append("<button>" + "장바구니 담기" + "</button>");
+  }
+});
