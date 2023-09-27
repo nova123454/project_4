f5bb51b49e982454bdf5ac11592f7b05e376eeb8
nova123454 <139088342+nova123454@users.noreply.github.com>
2023-09-27 16:35:27 +0900

    2023-09-27

--- js/sub.js
+++ js/sub.js
@@ -1,26 +1,3 @@
-$(function() {
-    $('button:nth-of-type(1)').click(function() {
-        $(".event_1").show();
-        $(".event_2").hide();
-        $(".event_3").hide();
-    });
-
-
-    $('button:nth-of-type(2)').click(function() {
-        $(".event_2").show();
-        $(".event_1").hide();
-        $(".event_3").hide();
-    });
-
-
-    $('button:nth-of-type(3)').click(function() {
-        $(".event_3").show();
-        $(".event_1").hide();
-        $(".event_2").hide();
-    });
-});
-
-
 $(function () {
     $.get("./sub_txt/txt1.txt", function (data) {
         console.log(data)
